ce1f158383027a70a1475997ec6114375fabba0e
oliveirafab16 <fabriciocarvalho932@gmail.com>
2020-12-09 13:26:04 -0300

    Projeto Finalizado

---
+++ Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string escolha, sair;
+
+            try
+            {
+                do
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("<------Calculadora Simples------>\n");
+
+                    Console.WriteLine("<------Operações Basicas------>");
+                    Console.WriteLine("Soma: +");
+                    Console.WriteLine("Subtração: -");
+                    Console.WriteLine("Multiplicação: *");
+                    Console.WriteLine("Divisão: /");
+                    Console.WriteLine("Potenciação: P");
+                    Console.WriteLine("Radiação (Raiz Quadrada): R\n");
+                    Console.Write("Escolha a Operação que Deseja Utilizar: ");
+                    escolha = Console.ReadLine().ToUpper();
+
+                    if (escolha == "+")
+                    {
+                        soma();
+                    }
+
+                    if (escolha == "-")
+                    {
+                        subtracao();
+                    }
+
+                    if (escolha == "*")
+                    {
+                        multiplicacao();
+                    }
+
+                    if (escolha == "/")
+                    {
+                        divisao();
+                    }
+
+                    if (escolha == "P")
+                    {
+                        potencia();
+                    }
+
+                    if (escolha == "R")
+                    {
+                        raiz();
+                    }
+
+                    Console.Write("Deseja Sair? S/N: ");
+                    sair = Console.ReadLine().ToUpper();
+                }
+                while (sair != "S" && sair != "SIM");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Não é possível dividir por zero.");
+                Console.WriteLine("Pressione Enter para sair!");
+                Console.ReadKey();
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Número inválido.");
+                Console.WriteLine("Pressione Enter para sair!");
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Número em formato inválido.");
+                Console.WriteLine("Pressione Enter para sair!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(); 
+                Console.WriteLine($"Erro genérico: {ex.Message}");
+                Console.WriteLine("Pressione Enter para sair!");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("Obrigado por ultilizar meu programa :)");
+        }
+        static void soma()
+        {
+            double numero1, numero2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1ºValor: ");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2ºValor: ");
+            numero2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = numero1 + numero2;
+            Console.WriteLine($"O resultado de {numero1} + {numero2} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void subtracao()
+        {
+            double numero1, numero2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1ºValor: ");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2ºValor: ");
+            numero2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = numero1 - numero2;
+            Console.WriteLine($"O resultado de {numero1} - {numero2} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void multiplicacao()
+        {
+            double numero1, numero2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1ºValor: ");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2ºValor: ");
+            numero2 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = numero1 * numero2;
+            Console.WriteLine($"O resultado de {numero1} * {numero2} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void divisao()
+        {
+            double numero1, numero2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1ºValor: ");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2ºValor: ");
+            numero2 = double.Parse(Console.ReadLine());
+
+            if (numero2 == 0)
+            {
+                throw new DivideByZeroException("Não é possível dividir por zero.");
+            }
+
+            Console.WriteLine();
+            resultado = numero1 / numero2;
+
+            Console.WriteLine($"O resultado de {numero1} / {numero2} = {resultado}");
+            Console.WriteLine();
+        }
+
+        static void potencia()
+        {
+            double numero1, numero2, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o 1ºValor: ");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2ºValor: ");
+            numero2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            resultado = numero1 * numero2 / 100;
+
+            Console.WriteLine($"O resultado de {numero1} * {numero2} / 100 = {resultado}%");
+            Console.WriteLine();
+        }
+
+        static void raiz()
+        {
+            double raizQuadrada, resultado;
+
+            Console.Clear();
+
+            Console.Write("Digite o Valor de Raiz Quadrada: ");
+            raizQuadrada = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            resultado = Math.Sqrt(raizQuadrada);
+            Console.WriteLine($"A raiz quadrada de {raizQuadrada} é = {Math.Round(resultado, 2)}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Calculadora
-Crie opções que permitam que o usuário efetue operações matemáticas 
+# Calculadora Dev#
+## O Programa
+
+- O programa deverá conter um menu de opções selecionáveis pelo usuário, que permitam que o usuário efetue operações matemáticas (o usuário digita os números desejados, e o programa exibe o resultado):
+
+- As quatro operações aritméticas básicas (soma, subtração, multiplicação e divisão);
+Exponenciação e radiciação;
+Inclua uma opção "Sobre" (ou "Créditos", como preferir) que exiba informações sobre o desenvolvedor e o link para o site do projeto.
+Inclua uma opção para o usuário sair do programa.
+---
+## _Screenshot_
+![Tela do Programa](tela.png)
+---
+## _Download_
+
+Baixe o aquivo abaixo. Descompacte na pasta desejada.
+
+🗃 [Download do arquivo.zip](dist/ProjetoDevCSharp.zip)
+
+Execute utilizando o comando abaixo:
+
+
+- dotnet Calculadora.dll
+
+
+
+ou, de duplo-click encima do icone programa se estiver no Windows.
+
+---
+## Agradecimentos
+
+- [Etec Adolpho Berezin](http://eteab.com.br/cms/)
+- [Prof. Ermogenes Palacio](https://github.com/ermogenes)
+- [Prof. Diego Neri](https://github.com/diegoneri)
+- [Dev c#](https://github.com/OliveiraFab16/Calculadora)
+
+---
+
+Todos os direito reservados.
